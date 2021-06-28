c50cfde95bfb9c3c56bf5e3e959b435e066dfc10
krisstallino <krisstallino@gmail.com>
2021-06-28 17:07:16 +0300

    updated

--- CreditPaymentService.java
+++ CreditPaymentService.java
@@ -1,9 +1,9 @@
 public class CreditPaymentService {
     public long calculate(long credit, int month, double percent) {
         double interest = percent / 100 / 12;
-        double amount = (1 + interest);
+        double amount = 1 + interest;
         double degree = month;
-        var answer = (Math.pow(amount, degree));
+        var answer = Math.pow(amount, degree);
         double creditPayment;
         creditPayment = credit * (interest + (interest / (answer - 1)));
         return (long) creditPayment;
